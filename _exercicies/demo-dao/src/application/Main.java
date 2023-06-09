461e12f79053c0170ed2102e08b0e4e8dc5b92dc
Breno <breno.am2004@gmail.com>
2023-06-08 23:46:45 -0300

    findByDepartment method implemented

--- _exercicies/demo-dao/src/application/Main.java
+++ _exercicies/demo-dao/src/application/Main.java
@@ -2,7 +2,10 @@ package application;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
+import model.entities.Department;
 import model.entities.Seller;
+
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
@@ -13,5 +16,13 @@ public class Main {
         Seller seller = sellerDao.findById(3);
 
         System.out.println(seller);
+
+        Department department = new Department(2, "Exemplo");
+
+        List<Seller> list = sellerDao.findByDepartment(department);
+
+        for (Seller obj : list) {
+            System.out.println(obj);
+        }
     }
 }
--- _exercicies/demo-dao/src/model/dao/impl/SellerDaoJDBC.java
+++ _exercicies/demo-dao/src/model/dao/impl/SellerDaoJDBC.java
@@ -10,7 +10,10 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class SellerDaoJDBC implements SellerDao {
 
@@ -74,5 +77,45 @@ public class SellerDaoJDBC implements SellerDao {
     @Override
     public List<Seller> findAll() {
         return null;
+    }
+
+    @Override
+    public List<Seller> findByDepartment(Department department) {
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null;
+
+        try {
+            preparedStatement = connection.prepareStatement("SELECT seller.*, department.Name as DepName FROM seller INNER JOIN department ON seller.DepartmentId = department.Id WHERE DepartmentId = ? ORDER BY Name");
+
+            preparedStatement.setInt(1, department.getId());
+
+            resultSet = preparedStatement.executeQuery();
+
+            List<Seller> list = new ArrayList<>();
+            Map<Integer, Department> map = new HashMap<>();
+
+            while(resultSet.next()) {
+
+                Department dep = map.get(resultSet.getInt("DepartmentId"));
+
+                // Se não existir. Crie e armazene para que na próxima use o MESMO DEPARTAMENTO
+                /*
+                 * Isso faz com que não tenha um dep instanciado para cada seller, mas sim um único dep para todos os sellers relacionados
+                 */
+                if(dep == null) {
+                    dep = new Department(resultSet.getInt("DepartmentId"), resultSet.getString("DepName"));
+                    map.put(resultSet.getInt("DepartmentId"), dep);
+                }
+
+                Seller seller = new Seller(resultSet.getInt("Id"), resultSet.getString("Name"), resultSet.getString("Email"), resultSet.getDate("BirthDate"), resultSet.getDouble("BaseSalary"), dep);
+
+                list.add(seller);
+            }
+
+            return list;
+        }
+        catch(SQLException mySQLError) {
+            throw new DbException(mySQLError.getMessage());
+        }
     }
 }
