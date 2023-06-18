22a88e0820d7484c9a876ac7d3dcb10c6f7567b5
Breno <breno.am2004@gmail.com>
2023-06-17 23:35:04 -0300

    maven version on pom

--- java-jpa_hibernate-02/src/main/java/Main.java
+++ java-jpa_hibernate-02/src/main/java/Main.java
@@ -1,5 +1,12 @@
 /*
- *  A estrutura antiga era JAVA. Para utilizar o JPA, utilizamos o Maven project
+ * A estrutura antiga era JAVA. Para utilizar o JPA, utilizamos o Maven project
+ *
+ * Em pom.xml: por a versão do maven (LTS 20)
+ *  <properties>
+ *      <maven.compiler.source>20</maven.compiler.source>
+ *      <maven.compiler.target>20</maven.compiler.target>
+ *      <project.build.sourceEncoding>UTF-8</project.build.sourceEncoding>
+ *  </properties>
  */
 
 import entities.Person;
