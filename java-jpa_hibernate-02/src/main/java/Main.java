c8913c383d61a76f94c3c907e2f03532be07f444
Breno <breno.am2004@gmail.com>
2023-06-18 16:17:14 -0300

    monitoramento

--- java-jpa_hibernate-02/src/main/java/Main.java
+++ java-jpa_hibernate-02/src/main/java/Main.java
@@ -14,14 +14,54 @@
  * Ele procurará automaticamente
  *
  * Adicione em resources o META-INF (directory) e dentro dele o  persistence.xml
+ *
+ * Tudo isso que falei é uma das características da arquitetura de programas JPA, que seria os arquivos de configuração
+ * Além disso, há o mapeamento.
  */
 
 import entities.Person;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 public class Main {
     public static void main(String[] args) {
-        Person person = new Person(0, "Breno", "breno@gmail.com");
+        /*
+         * O JPA monitora
+         */
+        Person person = new Person(null, "Breno", "breno@gmail.com");
 
-        System.out.println(person);
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("persistence-apelido");
+
+        // Operações são feitas com EntityManager
+        EntityManager em = emf.createEntityManager();
+
+        /*
+         * Começa operação
+         * em.getTransaction().begin();
+         *
+         * Adiciona row ->
+         * em.persist(person);
+         *
+         * Executa operação
+         * em.getTransaction().commit();
+         *
+         * OBS: getTransaction com begin e commit SEMPRE que você for mudar o banco de dados
+         *
+         * Para remover
+         * em.remove(objeto/row)
+         *
+         * Para usar o remove, precisamos que o objeto esteja sendo monitorado. Para isso, o objeto removido deve ter
+         * sido recuperado com find ou então que você tenha acabado de inserir com persist sem fechar o em (em.close())
+         */
+
+        // Busca dado com base no id
+        Person pessoaBusca = em.find(Person.class, 1);
+
+        System.out.println(pessoaBusca);
+
+        em.close();
+        emf.close();
     }
 }
--- java-jpa_hibernate-02/src/main/java/entities/Person.java
+++ java-jpa_hibernate-02/src/main/java/entities/Person.java
@@ -1,13 +1,30 @@
 package entities;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.io.Serializable;
 
+/*
+ * É o próprio JPA que cria as tabelas do banco, assim sendo, os atributos compreendem os nomes das colunas. Podemos
+ * mudar para @Column(atributo = [nome])
+ */
+
+// Indica que Person é uma entidade do banco
+@Entity
 public class Person implements Serializable {
     private static final long serialVersionUID = 1L;
 
+    // Indica que o id é a chave primária e a estratégia de geração desse valor é da identidade (++)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
     private String name;
     private String email;
+
+    public Person() {}
 
     public Person(Integer id, String name, String email) {
         this.id = id;
