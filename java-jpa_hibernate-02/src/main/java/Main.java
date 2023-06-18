df10552c6b2c18259f95b934cd7c5719b6aee597
Breno <breno.am2004@gmail.com>
2023-06-17 23:31:28 -0300

    maven project

---
+++ java-jpa_hibernate-02/src/main/java/Main.java
@@ -0,0 +1,11 @@
+/*
+ *  A estrutura antiga era JAVA. Para utilizar o JPA, utilizamos o Maven project
+ */
+
+import entities.Person;
+
+public class Main {
+    public static void main(String[] args) {
+        Person person = new Person(0, "Breno", "breno@gmail.com");
+    }
+}
---
+++ java-jpa_hibernate/src/Main.java
@@ -0,0 +1,7 @@
+import entities.Person;
+
+public class Main {
+    public static void main(String[] args) {
+        Person person = new Person(1, "Carlos", "carlos@gmail.com");
+    }
+}
