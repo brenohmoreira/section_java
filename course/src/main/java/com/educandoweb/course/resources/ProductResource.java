cb7a04f8e3ffd1ac5e81c012dbf35ce1b0585149
Breno <breno.am2004@gmail.com>
2023-06-27 22:19:08 -0300

    product implementation

---
+++ course/src/main/java/com/educandoweb/course/resources/ProductResource.java
@@ -0,0 +1,31 @@
+package com.educandoweb.course.resources;
+
+import com.educandoweb.course.entities.Product;
+import com.educandoweb.course.services.ProductService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/products")
+public class ProductResource {
+    private final ProductService productService;
+
+    public ProductResource(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Product>> findAll() {
+        return ResponseEntity.ok().body(productService.findAll());
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Product> findById(@PathVariable Long id) {
+        return ResponseEntity.ok().body(productService.findById(id));
+    }
+}
